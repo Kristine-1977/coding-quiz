5824502051d0e18015411b587c24c7bc41367d83
Kristine Horton <kristinehrtn@yahoo.com>
2020-10-28 21:42:06 -0600

    commit all changes

--- script.js
+++ script.js
@@ -1,47 +1,136 @@
-let timerEl = document.getElementById('timer-container');
-var secondsLeft = 300;
-
+<link rel="stylesheet" href="index.html"/>
+let currentQuestion = 0;
+let timerEl = document.querySelector(".navbar-text");
+var secondsLeft = 120;
+// timer information
 function setTime() {
   var timerInterval = setInterval(function() {
-    secondsLeft--;
-    timerEl.textContent = secondsLeft;
-
-    if(secondsLeft <= 0) {
+    timerEl.textContent = "Time:" + secondsLeft;
+    if(secondsLeft <= 0 || currentQuestion >= quizArr.length) {
       clearInterval(timerInterval);
-      }
-else {
+      score();
+      } else {
   secondsLeft--;
 }
   }, 1000);
 }
+// question display
+function questionDisplay(qtn, choice) {
+  console.log("question display");
+  myQuestionsEl.innerHTML = "";
+  var questionTitle = document.createElement("p");
+  var node =document.createTextNode(qtn);
+  questionTitle.appendChild(node);
+  myQuestionsEl.appendChild(questionTitle);
+// answer choices
+  var choiceAEl = document.createElement("li");
+  var choiceBEl = document.createElement("li");
+  var choiceCEl = document.createElement("li");
+  var questiongroup = document.createElement("ol");
 
-setTime();
+  var nodeA = document.createTextNode(choice[0]);
+  choiceAEl.appendChild(nodeA);
+  var nodeB = document.createTextNode(choice[1]);
+  choiceBEl.appendChild(nodeB);
+  var nodeC = document.createTextNode(choice[2]);
+  choiceCEl.appendChild(nodeC);
 
-function generateQuiz(questions, quizContainer, resultsContainer, submitButton){
-
-
-function showQuestions(questions, quizContainer){
-var output = [];
-
-var answers;
-
-for(var i=0; i<questions.length; i++){
-  answers = [];
-  for(letter in questions[i].answers){
-answers.forEachpush('<label>'+'input type'="radio" name="'question' +i+'value="'+ letter + ': '+ questions[i].answers[letter] + '</label>'
-);
-output.push(
-  `${`<div class="question">` + questions[i].question}<div><div class="answers">${answers.join('')}</div>`
-);
-  }
-  quizContainer.innerHtml = output.join('');
+ myQuestionsEl.appendChild(questiongroup);
+ questiongroup.appendChild(choiceAEl);
+ questiongroup.appendChild(choiceBEl);
+ questiongroup.appendChild(choiceCEl);
+ console.log("display" + qtn);
 }
-  
+// running quiz
+myQuestionsEl.onclick =function (event) {
+  let li = event.target.closest("li");
+  if(!li) return;
+  if (!myQuestionsEl.contains(li)) return;
+  if (li.textContent === quizArr[currentQuestion].answer) {
+    currentQuestion++;
+    runQuiz();
+  } else {
+    console.log("red");
+    if(secondsLeft < 30) {
+      secondsLeft = 0 
+      score();
+    } else {
+      secondsLeft = secondsLeft - 30;
+    }
+  currentQuestion++;
+  runQuiz();
+}
+};
+function runQuiz() {
+  if(currentQuestion < quizArr.length) {
+    questionDisplay(
+      quizArr[currentQuestion].questionText,
+      quizArr[currentQuestion].multipleChoice
+    );
+  } else {
+    score()
+  }
+}
+var scoreEl = document.querySelector(".score");
+function hideQuestions() {
+  myQuestionsEl.getElementsByClassName.display = "none";
+}
+function score() {
+  scoreEl.innerHTML = "";
+  hideQuestions();
+}
+var scoreHeader = document.createElement("h1");
+var node = document.createTextNode("The End");
+scoreHeader.appendChild(node);
+scoreEl.appendChild(scoreHeader);
 
-var myQuestions = [
-  {
-    question: "What is the symbol for termination?",
+var yourScore = document.createElement("p");
+var enterInitial = document.createElement("input");
+var submitButton = doucment.createElement("button");
+var tryAgain = document.createElement("a");
+var brk = document.createElement("br");
 
+var nodeA = document.createTextNode("Your score: " + secondsLeft);
+yourScore.appendChild(nodeA);
+enterInitial.setAttribute("placeholder", "your initials");
+enterInitial.setAttribute("maxlength", 4);
+enterInitial.setAttribute("id", "initialInput");
+
+submitButton.setAttribute("class", "submit");
+submitButton.setAttribute("type", "submit");
+submitButton.textContent = ("submit");
+
+var nodeC = document.createTextNode("Click here to start over");
+startOver.setAttribute("href", "index.html");
+startOver.setAttribute("class", "link");
+startOver.appendChild(nodeC);
+
+scoreEl.appendChild(yourScore);
+scoreEl.appendChild(enterInitial);
+scoreEl.appendChild(submitButton);
+scoreEl.appendChild(brk);
+scoreEl.appendChild(startOver);
+
+var submitButton = document.querySelector(".submit");
+submitButton.addeventlistner("click", function (event) {
+  event.preventDefault();
+  if (enterInitial.value.length < 1)
+  return;
+  else {
+    localStorage.setItem(document.getElementsById("initialInput").value, secondsLeft);
+    enterInitial.value = "";
+  }
+  doucment.querySelector(".submit").disabled = true;
+  })
+}
+// listen for the use to click 
+document.getElementById("start Button").addEventListener("click", setTime);
+document.getElementById("start Button").addEventListener("click", runQuiz);
+
+var myQuestionsEl = document.querySelector(".question") 
+quizArr = [{
+    questionText: "What is the symbol for termination?",
+    
     answers: {
 
       a: "semicolon",
@@ -51,36 +140,9 @@ var myQuestions = [
       c: "period"
     }
     .correctAnswer, "a"
-  };
-  userAnswer = (answerContainers[i].querySelector('input'[name=question'+i+]:checked)||{}).value;
-  if(userAnswer===questions[i].correctAnswer){
-    numCorrect++;
-    answerContainers[i].style.color = 'lightgreen';
-  }
-  else{
-    answerContainers[i].style.color = 'red';
-  }
-  resultsContainer.innerHtml = numCorrect + ' out of ' + questions.length;
-
-  resultsContainer.innerHtml = numCorrect +'out of' + questions.length
-  for(var i=0; i<questions.length; i++){
-    answers = [];
-    for(letter in questions[i].answers){
-      answers.forEachpush('<label>'+'input type'="radio" name="'question' +i+' value="'+ letter + ': '+ questions[i].answers[letter] + '</label>'
-      );
-      output.push(
-        `${`<div class="question">` + questions[i].question}<div><div class="answers">${answers.join('')}</div>`
-      );
-    }
-  function handleClick(event) {
-    console.log(event);
-  };
-  BroadcastChannel.addEventListner("click", handleClick);
-  submitButton.onclick = function(){
-    function showresults(questions, quizContainer, resultsContainer){
-    }
-
-  question, "What is another term used in place of Var?",
+  },
+ 
+  questionText, "What is another term used in place of Var?",
 
   answers, {
 
@@ -92,34 +154,7 @@ var myQuestions = [
 
     .correctAnswer, "c"
     },
-    
-    userAnswer = (answerContainers[i].querySelector('input'[name=question'+i+]:checked)||{}).value;
-    if(userAnswer===questions[i].correctAnswer){
-      numCorrect++;
-      answerContainers[i].style.color = 'lightgreen';
-    }
-    else{
-      answerContainers[i].style.color = 'red';
-      resultsContainer.innerHtml = numCorrect + ' out of ' + questions.length;
-
-    for(var i=0; i<questions.length; i++){
-      answers = [];
-      for(letter in questions[i].answers){
-        answers.forEachpush('<label>'+'input type'="radio" name="'question' +i+' value="'+ letter + ': '+ questions[i].answers[letter] + '</label>'
-        );
-        output.push(
-          `<div class="question">` + questions[i].question + '<div>' 
-        + '<div class="answers">' + answers.join('') + '</div>'
-        );
-      }
-      function handleClick(event) {
-        console.log(event);
-      };
-      BroadcastChannel.addEventListner("click", handleClick);
-      submitButton.onclick = function(){
-        function showresults(questions, quizContainer, resultsContainer){
-        }
-
+ 
     question, "What is the name of a piece of data that is described as a 'cubby hole' in class?",
 
     answers, {
@@ -132,35 +167,8 @@ var myQuestions = [
 
       },
     correctAnswer, "b"
-    userAnswer = (answerContainers[i].querySelector('input'[name=question'+i+]:checked)||{}).value;
-    if(userAnswer===questions[i].correctAnswer){
-      numCorrect++;
-      answerContainers[i].style.color = 'lightgreen';
-    }
-    else{
-      answerContainers[i].style.color = 'red';
-      resultsContainer.innerHtml = numCorrect + ' out of ' + questions.length;
-    for(var i=0; i<questions.length; i++){
-      answers = [];
-      for(letter in questions[i].answers){
-        answers.forEachpush(
-          '<label>'+'input type'="radio" name="'question' +i+'value="'
-          + letter + ': '+ questions[i].answers[letter] + '</label>'
-    );
-    output.push(
-      `<div class="question">` + questions[i].question + '<div>' 
-      + '<div class="answers">' + answers.join('') + '</div>'
-      );
-      }
-      function handleClick(event) {
-        console.log(event);
-      };
-      BroadcastChannel.addEventListner("click", handleClick);
-      submitButton.onclick = function(){
-        function showresults(questions, quizContainer, resultsContainer){
-        }
-
-    question, "What is the name of a tool to do something in Javascript?",
+   
+    questionText: "What is the name of a tool to do something in Javascript?",
 
     answers, {
 
@@ -174,33 +182,7 @@ var myQuestions = [
 
     correctAnswer, "b"
 
-    userAnswer = (answerContainers[i].querySelector('input'[name=question'+i+]:checked)||{}).value;
-    if(userAnswer===questions[i].correctAnswer){
-      numCorrect++;
-      answerContainers[i].style.color = 'lightgreen';
-    }
-    else{
-      answerContainers[i].style.color = 'red';
-      resultsContainer.innerHtml = numCorrect + ' out of ' + questions.length;
-    for(var i=0; i<questions.length; i++){
-      answers = [];
-      for(letter in questions[i].answers){
-        answers.forEachpush(
-          '<label>'+'input type'="radio" name="'question' +i+' value="`${letter}: ${questions[i].answers[letter]}</label>`
-  );
-  output.push(
-  `<div class="question">` + questions[i].question + '<div>' 
-  + '<div class="answers">' + answers.join('') + '</div>'
-  );
-      }
-      function handleClick(event) {
-        console.log(event);
-      };
-      BroadcastChannel.addEventListner("click", handleClick);
-      submitButton.onclick = function(){
-        function showresults(questions, quizContainer, resultsContainer){
-        }
-    question, "True or false,'Let' is more strict than 'var'.",
+      questionText: "True or false,'Let' is more strict than 'var'.",
 
     answers, {
 
@@ -211,36 +193,4 @@ var myQuestions = [
     },
     correctAnswer, "a"
   };
-userAnswer = (answerContainers[i].querySelector('input'[name=question]'+i+]:checked)||{}).value;
-if(userAnswer===questions[i].correctAnswer){
-  numCorrect++;
-  answerContainers[i].style.color = 'lightgreen';
-}
-else{
-  answerContainers[i].style.color = 'red';
-  resultsContainer.innerHtml = numCorrect + ' out of ' + questions.length;
-
-submitButton.onclick = function(){
-function showresults(questions, quizContainer, resultsContainer){
-}
-function handleClick(event) {
-  console.log(event);
 };
-BroadcastChannel.addEventListner("click", handleClick);
-var answerContainers = quizContainer.querySelectorAll('.answers');
-var userAnswer = '';
-var numCorrect = 0;
-// show the questions
-showQuestions(questions, quizContainer);
-
-submitButton.oneclick = function(){
-  showresults(questions, quizContainer, resultsContainer);
-}
-      function newFunction(quizContainer, output) {
-        quizContainer.innerHtml = output.join('');
-      }
-}
-var quizContainer = document.getElementById('quiz');
-var resultsContainer = document.getElementById('results');
-var submitButton = document.getElementById('submit');
-generateQuiz(myQuestions, quizContainer, resultsContainer, submitButton);
